74c72350151096fecea4f82279f6fd20d16a1a10
ninearif <ninearif@hotmail.com>
2017-02-28 19:36:46 +0700

    update key & remove wolf binarize

--- common/include/common/support.h
+++ common/include/common/support.h
@@ -12,6 +12,8 @@
 #define KEY_ENTER   13
 #define KEY_ESC     27
 #define KEY_N       110
+#define KEY_R       114
+#define KEY_S       115
 #else
 #define KEY_UP      2490368
 #define KEY_DOWN    2621440
